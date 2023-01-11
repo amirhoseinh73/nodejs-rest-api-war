25bbf9efd02cf1eb2ca62962a0e9ed60aeb0a658
amirhosein hasani <74850131+amirhoseinh73@users.noreply.github.com>
2023-01-11 15:47:25 +0330

    [FEAT]: change limit request size and remove unnecessary scene paths

--- app/config.js
+++ app/config.js
@@ -2,12 +2,23 @@ import dotenv from 'dotenv';
 import express from "express"
 import { fileURLToPath } from 'url';
 import path from "path"
+import bodyParser from "body-parser"
 
 dotenv.config()
 
 export const app = express()
 
 export const router = express.Router()
+
+app.use( bodyParser.json({
+  limit: '5000mb'
+}));
+
+app.use( bodyParser.urlencoded({
+  limit: '5000mb',
+  // parameterLimit: 100000,
+  extended: true
+}));
 
 export const APP_PORT = process.env.APP_PORT || 3000
 
@@ -17,18 +28,20 @@ export const JWT_SECRET = process.env.JWT_SECRET || ""
 
 export const jwt_blacklist = []
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
+export const __filename__ = fileURLToPath(import.meta.url);
+export const __dirname__ = path.dirname(__filename__);
 
-export const __dir_projects = `${__dirname}/../uploads/projects`
-export const __subdir_projects = {
+export const __dir_projects__ = `${__dirname__}/../uploads/projects`
+
+export const __subdir_projects__ = {
   "images"    : "images",
   "models"    : "models",
-  "projects"  : "projects",
-  "positions" : "positions",
-  "scales"    : "scales",
   "targets"   : "targets",
   "videos"    : "videos",
-  "active_target_names" : "active_target_names",
+  // "projects"  : "projects",
+  // "positions" : "positions",
+  // "scales"    : "scales",
+  // "active_target_names" : "active_target_names",
 }
-export const __dir_scene_data_ = "scene-data.json"
+
+export const __scene_data_file_name_ = "scene-data.json"
