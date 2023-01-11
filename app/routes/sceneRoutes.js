25d74beed9cedb27e396158654aacec4dc985716
amirhosein hasani <74850131+amirhoseinh73@users.noreply.github.com>
2023-01-11 15:46:15 +0330

    [FEAT]: add CRUD api routes for sceneController

--- app/routes/sceneRoutes.js
+++ app/routes/sceneRoutes.js
@@ -2,9 +2,17 @@ import { router } from "../config.js"
 import sceneController from "../controllers/sceneController.js"
 import isAuth from "../middlewares/isAuth.js"
 
-router.post("/save/path/:name", isAuth, sceneController.createProjectPath)
+router.get("/", isAuth, sceneController.readAllScenes)
 
+router.post("/", isAuth, sceneController.createScene)
+router.get("/:id", isAuth, sceneController.readScene)
+router.patch("/:id", isAuth, sceneController.updateScene)
+router.delete("/:id", isAuth, sceneController.deleteScene)
+
+router.post("/save/data/:id", isAuth, sceneController.createSceneData);
 router.get("/read/data/:id", isAuth, sceneController.readSceneData);
-router.post("/save/data/:id", isAuth, sceneController.saveSceneData);
+
+// router.get("/read/scale/:id", isAuth, sceneController.readScaleData);
+// router.post("/save/scale/:id", isAuth, sceneController.saveScaleData);
 
 export default router
